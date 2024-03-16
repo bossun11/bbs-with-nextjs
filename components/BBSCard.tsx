844b356c88f33629022ba48897749404179c5eac
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-16 16:53:05 +0900

    Style: 投稿を表示するためのカードを作成

---
+++ components/BBSCard.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+const BBSCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href={"/bbs-posts/1"} className="text-blue-500">
+          Learn more
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default BBSCard;
---
+++ components/BBSCardList.tsx
@@ -0,0 +1,14 @@
+import BBSCard from "./BBSCard";
+
+const BBSCardList = () => {
+  return (
+    <div className="grid lg:grid-cols-3 p-4 gap-4">
+      <BBSCard />
+      <BBSCard />
+      <BBSCard />
+      <BBSCard />
+    </div>
+  );
+};
+
+export default BBSCardList;
