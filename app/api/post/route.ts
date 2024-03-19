7d5d0305cc688f44b007ff7ef73a16c2f79a9c95
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-19 18:39:14 +0900

    Add: 投稿作成機能を実装

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -3,3 +3,17 @@ export async function GET() {
   const bbsAllData = await response.json();
   return Response.json(bbsAllData);
 }
+
+export async function POST(request: Request) {
+  const { title, content } = await request.json();
+
+  const response = await fetch(`${process.env.BACKEND_API_URL}/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, content }),
+  });
+
+  return response;
+}
---
+++ app/bbs-posts/create/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(2, { message: "タイトルは2文字以上で入力してください" })
+    .max(100, { message: "タイトルは100文字以下で入力してください" }),
+  content: z
+    .string()
+    .min(10, { message: "本文は10文字以上で入力してください" })
+    .max(1000, { message: "本文は1000文字以下で入力してください" }),
+});
+
+const CreateBBSPage = () => {
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
+  });
+
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    const { title, content } = value;
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_API_URL}/post`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+      router.push("/");
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-3 w-1/2 px-7"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>タイトル</FormLabel>
+              <FormControl>
+                <Input placeholder="タイトル" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="投稿内容"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CreateBBSPage;
